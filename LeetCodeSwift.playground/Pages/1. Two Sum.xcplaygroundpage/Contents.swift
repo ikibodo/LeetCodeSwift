4aa9a0df2859310955086ce1d4fa44e99b5dc235
kibodo <kibodo@ya.ru>
2025-10-30 19:26:18 +0100

    1. Two Sum

---
+++ LeetCodeSwift.playground/Pages/1. Two Sum.xcplaygroundpage/Contents.swift
@@ -0,0 +1,12 @@
+func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
+    var numToIndex = [Int: Int]()
+    
+    for (i, num) in nums.enumerated() {
+        let complement = target - num
+        if let j = numToIndex[complement] {
+            return [j, i]
+        }
+        numToIndex[num] = i
+    }
+    return []
+}
