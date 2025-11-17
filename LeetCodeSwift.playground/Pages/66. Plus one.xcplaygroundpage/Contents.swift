eef6b538de657ef3dfa94d0c720aba6c42ab62ef
kibodo <kibodo@ya.ru>
2025-11-17 22:45:33 +0100

    66. Plus one

---
+++ LeetCodeSwift.playground/Pages/66. Plus one.xcplaygroundpage/Contents.swift
@@ -0,0 +1,13 @@
+func plusOne(_ digits: [Int]) -> [Int] {
+    var digits = digits
+    for i in stride(from: digits.count - 1, through: 0, by: -1) {
+        if digits[i] < 9 {
+            digits[i] += 1
+            return digits
+        }
+        if digits[i] == 9 {
+            digits[i] = 0
+        }
+    }
+    return [1] + digits
+}
