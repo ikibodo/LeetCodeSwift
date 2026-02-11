b4e4c8486a425133d6cee5d4b81bb4cb326feaa1
kibodo <kibodo@ya.ru>
2026-02-11 20:36:24 +0100

    228. Summary Ranges

---
+++ LeetCodeSwift.playground/Pages/228. Summary Ranges.xcplaygroundpage/Contents.swift
@@ -0,0 +1,17 @@
+func summaryRanges(_ nums: [Int]) -> [String] {
+    var result = [String]()
+    var i = 0
+    while i < nums.count {
+        let start = i
+        while i + 1 < nums.count && nums[i + 1] == nums[i] + 1 {
+            i += 1
+        }
+        if start == i {
+            result.append("\(nums[start])")
+        } else {
+            result.append("\(nums[start])->\(nums[i])")
+        }
+        i += 1
+    }
+    return result
+}
