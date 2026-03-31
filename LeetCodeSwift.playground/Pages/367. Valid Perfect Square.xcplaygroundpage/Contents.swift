f2c9357a0ccac310d98caa92c5f4186ee64786cf
kibodo <kibodo@ya.ru>
2026-03-31 23:01:11 +0200

    367. Valid Perfect Square - optimized solution (Binary Search)

--- LeetCodeSwift.playground/Pages/367. Valid Perfect Square.xcplaygroundpage/Contents.swift
+++ LeetCodeSwift.playground/Pages/367. Valid Perfect Square.xcplaygroundpage/Contents.swift
@@ -1,9 +1,31 @@
+// Time: O(n)
+// Space: O(1)
 func isPerfectSquare(_ num: Int) -> Bool {
     if num == 1 { return true }
     let left = 1
     let right = num / 2
     for i in left...right {
         if i * i == num { return true }
+    }
+    return false
+}
+
+// Time: O(log n)
+// Space: O(1)
+func isPerfectSquareBS(_ num: Int) -> Bool {
+    if num == 1 { return true }
+    var left = 1
+    var right = num / 2
+    
+    while left <= right {
+        let mid = left + (right - left) / 2
+        if mid == num / mid && num % mid == 0 {
+            return true
+        } else if mid < num / mid {
+            left = mid + 1
+        } else {
+            right = mid - 1
+        }//*
     }
     return false
 }
