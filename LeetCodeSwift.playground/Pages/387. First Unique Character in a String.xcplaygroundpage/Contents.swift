fe5d3d8cf05aa12cddd1786d401e85f4c0324706
kibodo <kibodo@ya.ru>
2026-04-10 22:49:25 +0200

    387. First Unique Character in a String

---
+++ LeetCodeSwift.playground/Pages/387. First Unique Character in a String.xcplaygroundpage/Contents.swift
@@ -0,0 +1,15 @@
+func firstUniqChar(_ s: String) -> Int {
+    var charCount = [Character: Int]()
+    
+    for char in s {
+        charCount[char, default: 0] += 1
+    }
+    
+    for (index, char) in s.enumerated() {
+        if charCount[char] == 1 {
+            return index
+        }
+    }
+    
+    return -1
+}
