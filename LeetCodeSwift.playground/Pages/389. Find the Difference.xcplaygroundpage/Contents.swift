80cc4c7394b9aa6c2aac4891ac8a70ba1c67a4d5
kibodo <kibodo@ya.ru>
2026-04-22 23:09:52 +0200

    389. Find the Difference - alternative solution using sum

--- LeetCodeSwift.playground/Pages/389. Find the Difference.xcplaygroundpage/Contents.swift
+++ LeetCodeSwift.playground/Pages/389. Find the Difference.xcplaygroundpage/Contents.swift
@@ -1,6 +1,17 @@
+// Time: O(n)
+// Space: O(1)
 func findTheDifference(_ s: String, _ t: String) -> Character {
     var xor: UInt8 = 0
     for c in s.utf8 { xor ^= c }
     for c in t.utf8 { xor ^= c }
     return Character(UnicodeScalar(xor))
+}
+
+// Time: O(n)
+// Space: O(1)
+func findTheDifferenceSum(_ s: String, _ t: String) -> Character {
+    var sum = 0
+    for ch in t.unicodeScalars {  sum += Int(ch.value) }
+    for ch in s.unicodeScalars { sum -= Int(ch.value) }
+    return Character(UnicodeScalar(sum)!)
 }
