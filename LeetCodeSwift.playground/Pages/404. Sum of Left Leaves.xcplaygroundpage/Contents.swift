5e0019f36f32d989c267ba16b3b299e07e50e185
kibodo <kibodo@ya.ru>
2026-04-30 21:59:47 +0200

    404. Sum of Left Leaves - iterative solution

--- LeetCodeSwift.playground/Pages/404. Sum of Left Leaves.xcplaygroundpage/Contents.swift
+++ LeetCodeSwift.playground/Pages/404. Sum of Left Leaves.xcplaygroundpage/Contents.swift
@@ -11,6 +11,8 @@ class TreeNode {
     }
 }
 
+// Time: O(n)
+// Space: O(n) worst case, O(log n) average
 func sumOfLeftLeaves(_ root: TreeNode?) -> Int {
     guard let node = root else { return 0 }
     var sum = 0
@@ -18,4 +20,25 @@ func sumOfLeftLeaves(_ root: TreeNode?) -> Int {
         sum += left.val
     }
     return sum + sumOfLeftLeaves(node.left) + sumOfLeftLeaves(node.right)
+}
+
+// Time: O(n)
+// Space: O(n) worst case, O(log n) average
+func sumOfLeftLeavesIterative(_ root: TreeNode?) -> Int {
+    guard let root = root else { return 0 }
+    var stack: [TreeNode] = [root]
+    var result = 0
+    while let node = stack.popLast() {
+        if let left = node.left {
+            if left.left == nil && left.right == nil {
+                result += left.val
+            } else {
+                stack.append(left)
+            }
+        }
+        if let right = node.right {
+            stack.append(right)
+        }
+    }
+    return result
 }
