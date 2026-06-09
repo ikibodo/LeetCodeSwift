9470cd9d0ed1268781f5b8a5031b6c14f20b2919
kibodo <kibodo@ya.ru>
2026-06-09 23:39:38 +0200

    461. Hamming Distance - XOR + bit shift solution

--- LeetCodeSwift.playground/Pages/461. Hamming Distance.xcplaygroundpage/Contents.swift
+++ LeetCodeSwift.playground/Pages/461. Hamming Distance.xcplaygroundpage/Contents.swift
@@ -4,3 +4,19 @@
 func hammingDistance(_ x: Int, _ y: Int) -> Int {
     return (x ^ y).nonzeroBitCount
 }
+
+
+// Time: O(32) = O(1)
+// Space: O(1)
+
+func hammingDistance1(_ x: Int, _ y: Int) -> Int {
+    var n = x ^ y
+    var count = 0
+    
+    while n > 0 {
+        count += n & 1
+        n >>= 1
+    }
+    
+    return count
+}
